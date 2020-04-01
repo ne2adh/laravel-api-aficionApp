b31404dbeae1e559ae421cf08cd96223e16250a9
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2020-03-31 21:56:09 -0400

    api authentication

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+ */
+
+//Route::get('list', 'UsersController@list');
 
 
-Route::get('list', 'UsersController@list');
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'API\UserController@details');
+});
